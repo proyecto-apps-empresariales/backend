a4a6e2e35a56c8da4c7c6989e43ccd4376777580
Mateo Hurtado <hurtadotorresmateo@gmail.com>
2026-03-24 19:04:09 -0500

    ADD codigo completo de Rol

--- demoAPI/src/main/java/com/proyecto_backend/demoAPI/persistencia/repositorios/RolRepository.java
+++ demoAPI/src/main/java/com/proyecto_backend/demoAPI/persistencia/repositorios/RolRepository.java
@@ -1,5 +1,7 @@
 package com.proyecto_backend.demoAPI.persistencia.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import com.proyecto_backend.demoAPI.persistencia.entidades.Rol;
 
 @Repository
 public interface RolRepository extends JpaRepository<Rol, Long>{
+    
+    // Metodo para buscar un rol por nombre:
+    public Optional<Rol> findByNombre (String nombre);
     
 }
